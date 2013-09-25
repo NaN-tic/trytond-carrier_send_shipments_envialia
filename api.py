66a7d3f8067376621ac8b643d5ea75047df1c4e7
resteve <resteve@zikzakmedia.com>
2013-09-25 18:32:28 +0200

    Envialia test

--- api.py
+++ api.py
@@ -5,6 +5,13 @@ from trytond.model import fields
 from trytond.pool import PoolMeta
 from trytond.pyson import Eval
 
+try:
+    from envialia.picking import *
+except ImportError:
+    message = 'Unable to import Envialia: pip install envialia'
+    logging.getLogger('envialia').error(message)
+    raise Exception(message)
+
 __all__ = ['CarrierApi']
 __metaclass__ = PoolMeta
 
@@ -24,3 +31,18 @@ class CarrierApi:
         res = super(CarrierApi, cls).get_carrier_app()
         res.append(('envialia','Envialia'))
         return res
+
+    def test_envialia(self, api):
+        '''
+        Test Envialia connection
+        :param api: obj
+        '''
+        with API(api.envialia_agency, api.username, api.password, api.debug) \
+                as envialia_api:
+            test = envialia_api.test_connection()
+            if test.get('error'):
+                self.raise_user_error('connection_error')
+            elif test.get('session'):
+                self.raise_user_error('connection_successfully')
+            else:
+                self.raise_user_error('connection_error')

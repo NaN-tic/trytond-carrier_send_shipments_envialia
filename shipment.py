daa0b93578da3b27f2a3eef62040cee42724d88a
resteve <resteve@zikzakmedia.com>
2014-03-26 09:27:30 +0100

    Redesing send shipment and label methods

--- shipment.py
+++ shipment.py
@@ -1,10 +1,13 @@
 # This file is part of the carrier_send_shipments module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains
 # the full copyright notices and license terms.
-from trytond.pool import PoolMeta
+from trytond.pool import Pool, PoolMeta
+from trytond.transaction import Transaction
 from envialia.picking import Picking
 from trytond.modules.carrier_send_shipments.tools import unaccent
+from base64 import decodestring
 import logging
+import tempfile
 
 __all__ = ['ShipmentOut']
 __metaclass__ = PoolMeta
@@ -14,45 +17,61 @@ class ShipmentOut:
     __name__ = 'stock.shipment.out'
 
     @classmethod
-    def send_envialia(self, api, shipments, service):
+    def send_envialia(self, api, shipments):
         '''
         Send shipments out to envialia
         :param api: obj
         :param shipments: list
-        :param service: obj
+        Return references, labels, errors
         '''
+        pool = Pool()
+        CarrierApi = pool.get('carrier.api')
+
+        references = []
+        labels = []
+        errors = []
+
         agency = api.envialia_agency
         customer = api.username
         password = api.password
         debug = api.debug
 
-        with Picking(agency, customer, password, debug) as picking_api:
+        default_service = CarrierApi.get_default_carrier_service(api)
 
+        with Picking(agency, customer, password, debug) as picking_api:
             for shipment in shipments:
+                service = shipment.carrier_service or default_service
+
+                notes = ''
+                if shipment.carrier_notes:
+                    notes = CarrierApi.carrier_unaccent(shipment.carrier_notes)
+
+                packages = shipment.number_packages
+                if packages == 0:
+                    packages = 1
+
                 data = {}
                 data['agency_cargo'] = agency
                 data['agency_origin'] = customer
                 if not api.reference:
                     data['reference'] = shipment.code
-                data['service_code'] = service.code
+                data['service_code'] = str(service.code)
                 data['company_name'] = api.company.rec_name
                 data['company_code'] = customer
                 data['company_phone'] = api.phone
-                data['customer_name'] = shipment.customer.name
-                data['customer_contact_name'] = (shipment.delivery_address.name
-                    or shipment.customer.name)
-                data['customer_street'] = unaccent(
-                    shipment.delivery_address.street)
-                data['customer_city'] = unaccent(
-                    shipment.delivery_address.city)
+                data['customer_name'] = unaccent(shipment.customer.name)
+                data['customer_contact_name'] = unaccent((shipment.delivery_address.name
+                    or shipment.customer.name))
+                data['customer_street'] = unaccent(shipment.delivery_address.street)
+                data['customer_city'] = unaccent(shipment.delivery_address.city)
                 data['customer_zip'] = shipment.delivery_address.zip
                 data['customer_phone'] = shipment.delivery_address.phone or ''
-                data['document'] = shipment.number_packages
+                data['document'] = packages
                 if shipment.carrier_cashondelivery:
                     data['cash_ondelivery'] = shipment.carrier_cashondelivery
                     data['total'] = str(self.get_carrier_price_total(shipment))
                 data['ref'] = shipment.code
-                data['notes'] = shipment.comment
+                data['notes'] = notes
 
                 # Send shipment data to carrier
                 envialia = picking_api.create(data)
@@ -69,18 +88,27 @@ class ShipmentOut:
                         })
                     logging.getLogger('envialia').info(
                         'Send shipment %s' % (shipment.code))
+                    references.append(shipment.code)
                 if envialia and envialia.get('error'):
                     error = envialia.get('error')
                     logging.getLogger('envialia').error(
                         'Not send shipment %s. %s' % (shipment.code, error))
+                    errors.append(shipment.code)
+
+                labels += self.print_labels_envialia(api, shipments)
+
+        return references, labels, errors
 
     @classmethod
-    def print_labels_envialia(cls, shipments, api):
+    def print_labels_envialia(cls, api, shipments):
         agency = api.envialia_agency
         username = api.username
         password = api.password
         debug = api.debug
+
         labels = []
+        dbname = Transaction().cursor.dbname
+
         with Picking(agency, username, password, debug) as shipment_api:
             for shipment in shipments:
                 if not shipment.carrier_tracking_ref:
@@ -93,12 +121,20 @@ class ShipmentOut:
                 data['agency_origin'] = data['agency_cargo'] = agency
                 reference = shipment.carrier_tracking_ref
 
-                envialia = shipment_api.label(reference, data)
-                if not envialia:
+                label = shipment_api.label(reference, data)
+                if not label:
                     logging.getLogger('carrier_send_shipment_envialia').error(
                         'Label for shipment %s is not available from Envialia.'
                         % shipment.code)
                     continue
-                labels.append(envialia)
-                cls.write(shipments, {'printed': True})
+                with tempfile.NamedTemporaryFile(
+                        prefix='%s-envialia-%s-' % (dbname, reference),
+                        suffix='.pdf', delete=False) as temp:
+                    temp.write(decodestring(label)) # Envialia PDF file
+                logging.getLogger('envialia').info(
+                    'Generated tmp label %s' % (temp.name))
+                temp.close()
+                labels.append(temp.name)
+            cls.write(shipments, {'carrier_printed': True})
+
         return labels

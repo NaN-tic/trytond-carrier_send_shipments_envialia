e98eb57b23a852a060eb0039a3019ca36c24c9dd
resteve <resteve@zikzakmedia.com>
2014-07-15 12:30:54 +0200

    Send Shipment: send date and employee

--- shipment.py
+++ shipment.py
@@ -93,8 +93,8 @@ class ShipmentOut:
                         'carrier_tracking_ref': reference,
                         'carrier_service': service,
                         'carrier_delivery': True,
-                        'send_date': shipment.send_date,
-                        'send_employee': shipment.send_employee,
+                        'carrier_send_date': ShipmentOut.get_carrier_date(),
+                        'carrier_send_employee': ShipmentOut.get_carrier_employee() or None,
                         })
                     logging.getLogger('envialia').info(
                         'Send shipment %s' % (shipment.code))

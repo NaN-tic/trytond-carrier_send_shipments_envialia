8e96e082929280e5ca9d7bb2b83506cadb9ae462
resteve <resteve@zikzakmedia.com>
2015-08-19 23:23:24 +0200

    Remove safe eval

--- api.py
+++ api.py
@@ -1,9 +1,9 @@
-#This file is part carrier_send_shipments module for Tryton.
+#This file is part carrier_send_shipments_envialia module for Tryton.
 #The COPYRIGHT file at the top level of this repository contains 
 #the full copyright notices and license terms.
 from trytond.model import fields
 from trytond.pool import PoolMeta
-from trytond.pyson import Eval
+from trytond.pyson import Eval, Not, Equal
 import logging
 
 try:
@@ -32,6 +32,13 @@ class CarrierApi:
         res = super(CarrierApi, cls).get_carrier_app()
         res.append(('envialia','Envialia'))
         return res
+
+    @classmethod
+    def view_attributes(cls):
+        return super(CarrierApi, cls).view_attributes() + [
+            ('//page[@id="envialia"]', 'state', {
+                    'invisible': Not(Equal(Eval('method'), 'envialia')),
+                    })]
 
     def test_envialia(self, api):
         '''

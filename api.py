6f76d048c84a2e7ebaa2f900fc95e52fa6497677
Raimon Esteve <raimon@nan-tic.com>
2016-11-30 23:18:31 +0100

    Replace test_connection to class method

--- api.py
+++ api.py
@@ -1,5 +1,5 @@
 #This file is part carrier_send_shipments_envialia module for Tryton.
-#The COPYRIGHT file at the top level of this repository contains 
+#The COPYRIGHT file at the top level of this repository contains
 #the full copyright notices and license terms.
 from trytond.model import fields
 from trytond.pool import PoolMeta
@@ -10,7 +10,7 @@ try:
     from envialia.picking import *
 except ImportError:
     logger = logging.getLogger(__name__)
-    message = 'Install Envialia from Pypi: pip install envialia'
+    message = 'Install Envialia: pip install envialia'
     logger.error(message)
     raise Exception(message)
 
@@ -22,13 +22,11 @@ class CarrierApi:
     __name__ = 'carrier.api'
     envialia_agency = fields.Char('Agency', states={
             'required': Eval('method') == 'envialia',
-            }, help='Envialia Agency')
+        }, help='Envialia Agency')
 
     @classmethod
     def get_carrier_app(cls):
-        '''
-        Add Carrier Envialia APP
-        '''
+        'Add Carrier Envialia APP'
         res = super(CarrierApi, cls).get_carrier_app()
         res.append(('envialia','Envialia'))
         return res
@@ -40,17 +38,15 @@ class CarrierApi:
                     'invisible': Not(Equal(Eval('method'), 'envialia')),
                     })]
 
-    def test_envialia(self, api):
-        '''
-        Test Envialia connection
-        :param api: obj
-        '''
+    @classmethod
+    def test_envialia(cls, api):
+        'Test Envialia connection'
         with API(api.envialia_agency, api.username, api.password, api.debug) \
                 as envialia_api:
             test = envialia_api.test_connection()
             if test.get('error'):
-                self.raise_user_error('connection_error')
+                cls.raise_user_error('connection_error')
             elif test.get('session'):
-                self.raise_user_error('connection_successfully')
+                cls.raise_user_error('connection_successfully')
             else:
-                self.raise_user_error('connection_error')
+                cls.raise_user_error('connection_error')
